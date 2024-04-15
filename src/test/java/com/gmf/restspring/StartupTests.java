53bd0af6aed6cc84ebb53ea1b184a33208bc8174
Guilherme <guilherme_riba@hotmail.com>
2024-04-15 08:48:50 -0300

    Create project

---
+++ src/test/java/com/gmf/restspring/StartupTests.java
@@ -0,0 +1,13 @@
+package com.gmf.restspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
