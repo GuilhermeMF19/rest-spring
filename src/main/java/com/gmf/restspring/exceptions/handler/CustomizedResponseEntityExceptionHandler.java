e32efcd2ae7cafcc4e03579996ca4f70189af451
Guilherme <guilherme_riba@hotmail.com>
2024-04-17 20:30:22 -0300

    Create InvalidJwtAuthenticationException and implements handler method

--- src/main/java/com/gmf/restspring/exceptions/handler/CustomizedResponseEntityExceptionHandler.java
+++ src/main/java/com/gmf/restspring/exceptions/handler/CustomizedResponseEntityExceptionHandler.java
@@ -11,6 +11,7 @@ import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import com.gmf.restspring.exceptions.ExceptionResponse;
+import com.gmf.restspring.exceptions.InvalidJwtAuthenticationException;
 import com.gmf.restspring.exceptions.RequiredObjectIsNullException;
 import com.gmf.restspring.exceptions.ResourceNotFoundException;
 
@@ -34,5 +35,11 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
 	public final ResponseEntity<ExceptionResponse> handleBadRequestExceptions(Exception ex, WebRequest request) {
 		ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(false));
 		return new ResponseEntity<>(exceptionResponse,HttpStatus.BAD_REQUEST);
+	}
+
+	@ExceptionHandler(InvalidJwtAuthenticationException.class)
+	public final ResponseEntity<ExceptionResponse> handleInvalidJwtAuthenticationException(Exception ex, WebRequest request) {
+		ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(false));
+		return new ResponseEntity<>(exceptionResponse,HttpStatus.FORBIDDEN);
 	}
 }
