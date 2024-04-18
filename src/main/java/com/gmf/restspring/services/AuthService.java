5fa1cfa5fbfb4fde1b20dd871d990c7397e3313c
Guilherme <guilherme_riba@hotmail.com>
2024-04-18 09:36:48 -0300

    Create AuthService and delete JwtConfigurer(deprecated)

---
+++ src/main/java/com/gmf/restspring/services/AuthService.java
@@ -0,0 +1,47 @@
+package com.gmf.restspring.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.gmf.restspring.data.vo.v1.security.AccountCredentialsVO;
+import com.gmf.restspring.data.vo.v1.security.TokenVO;
+import com.gmf.restspring.repositories.UserRepository;
+import com.gmf.restspring.security.jwt.JwtTokenProvider;
+
+@Service
+public class AuthService {
+
+	@Autowired
+	private AuthenticationManager authenticationManager;
+	
+	@Autowired
+	private JwtTokenProvider tokenProvider;
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public ResponseEntity signin(AccountCredentialsVO data) {
+		try {
+			var username = data.getUsername();
+			var password = data.getPassword();
+			authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username,password));
+			
+			var user = repository.findByUsername(username);
+			
+			var tokenResponse = new TokenVO();
+			if (user != null) {
+				tokenResponse = tokenProvider.createAccessToken(username, user.getRoles());
+			} else {
+				throw new UsernameNotFoundException("Username " + username + " not found!");
+			}
+			return ResponseEntity.ok(tokenResponse);
+		} catch (Exception e) {
+			throw new BadCredentialsException("Invalid username/password supplied!");
+		}
+	}
+}
